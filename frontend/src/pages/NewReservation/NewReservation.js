bb15e8bd82fdb550d85a1f55956983d99c9743ef
Radoslav Banik <xbanik@fi.muni.cz>
2024-05-03 02:47:01 +0200

    new reservation route page

---
+++ frontend/src/pages/NewReservation/NewReservation.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { API_URL_BASE, API_URL_GET_DOCTORS } from '../../constants/ApiUrls';
+
+import axios from 'axios';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './NewReservation.css'
+
+import ReservationForm from '../../components/ReservationForm/ReservationForm';
+import NavBar from '../../components/NavBar/NavBar';
+
+
+const api = axios.create({baseURL: API_URL_BASE, withCredentials: true});
+
+const NewReservation = () => {
+    
+    
+    return (
+        <div className='reservation-page'>
+            <NavBar></NavBar>
+            <ReservationForm></ReservationForm>
+        </div>
+    );
+
+}
+
+export default NewReservation
