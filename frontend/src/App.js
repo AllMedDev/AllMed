db6e272384155de41487bf5c6a321d6f3636bb37
Radoslav Banik <xbanik@fi.muni.cz>
2024-05-04 10:18:36 +0200

    add future/past reservations main routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,37 +5,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 import TopScrollButton from './components/TopScrollButton/TopScrollButton';
-import Login from "./pages/Login/Login";
-import RegisterDoctor from "./pages/DoctorRegister/DoctorRegister";
-import RegisterPatient from "./pages/PatientRegister/PatientRegister";
-import Profile from "./pages/Profile/Profile"
 
-import { SITE_URL_DOCTORS, 
-    SITE_URL_PROFILE, 
-    SITE_URL_APPOINTMENTS_FUTURE, 
-    SITE_URL_APPOINTMENTS_PAST 
-} from './constants/SiteUrls';
+import LoginPage from "./pages/Login/Login";
+import RegisterDoctorPage from "./pages/DoctorRegister/DoctorRegister";
+import RegisterPatientPage from "./pages/PatientRegister/PatientRegister";
+import ProfilePage from "./pages/Profile/Profile"
+import DoctorsPage from "./pages/Doctors/Doctors";
+import ReservationFormPage from './pages/NewReservation/NewReservation';
+import PastAppointments from './pages/PastAppointments/PastAppointments';
+import FutureAppointments from './pages/FutureAppointments/FutureAppointments';
 
-// TBD
-// import PastAppointments from './pages/PastAppointments/PastAppointments';
-// import FutureAppointments from './pages/FutureAppointments/FutureAppointments';
-import Doctors from "./pages/Doctors/Doctors";
-import Homepage from './pages/Profile/Profile';
 
 const App = () => {    
     return (
         <div className='gradient-background'>
             <Router>
                 <Routes>
-                    <Route exact path="/" element={<Login />} />
-                    <Route exact path="/login" element={<Login />} />
-                    <Route path="/profil" element={<Profile />} />
-                    <Route path="/registracia-lekar" element={<RegisterDoctor /> } />
-                    <Route path="/registracia-pacient" element={<RegisterPatient /> } />
-                    <Route path="/lekari" element={<Doctors />} />
-                    {/* TBD */}
-                    {/* <Route path="/planovane-rezervacie"element={<PastAppointments />} />
-                    <Route path="/historia-rezervacii" element={<FutureAppointments />} /> */}
+                    <Route exact path="/" element={<LoginPage />} />
+                    <Route exact path="/login" element={<LoginPage />} />
+                    <Route path="/profil" element={<ProfilePage />} />
+                    <Route path="/registracia-lekar" element={<RegisterDoctorPage /> } />
+                    <Route path="/registracia-pacient" element={<RegisterPatientPage /> } />
+                    <Route path="/lekari" element={<DoctorsPage />} />
+                    <Route path="/nova-rezervacia" element={<ReservationFormPage />} />
+                    <Route path="/planovane-rezervacie"element={<FutureAppointments />} />
+                    <Route path="/historia-rezervacii" element={<PastAppointments />} />
                 </Routes>
                 <TopScrollButton />
             </Router>
